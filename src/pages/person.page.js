42bbdd61463fee4c18604793b572449785b664a3
Doctopus <tapasapt@gmail.com>
2024-01-14 03:03:59 +0530

    People table implemented

--- src/pages/person.page.js
+++ src/pages/person.page.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import axios from 'axios'
-import { useMatches } from 'react-router-dom'
+import React from "react";
+import axios from 'axios';
+import { useMatches } from "react-router-dom";
 
 const Person = () => {
-  const matches = useMatches()
-  const id = matches[0].params.id
-  console.log(matches)
-  const [person, setPerson] = React.useState([])
+  const matches = useMatches();
+  const id = matches[0].params.id;
+  console.log(matches);
+  const [person, setPerson] = React.useState([]);
   const getPerson = () => {
     axios
       .get(`http://localhost:3000/people/${id}`)
       .then((result) => {
-        setPerson(result.data)
+        setPerson(result.data);
+        console.log(result);
       })
       .catch((err) => {
-        setPerson([err])
-      })
-  }
+        setPerson([err]);
+      });
+  };
   React.useEffect(() => {
-    getPerson()
-  }, [])
+    getPerson();
+  }, []);
 
-  return <div> {person?.firstName} </div>
-}
-export { Person }
+  return (
+    <table>
+      <tr>
+        <td>{person.firstName}</td>
+        <td>{person.lastName}</td>
+        <td>{person.dob}</td>
+        <td>{person.gender}</td>
+      </tr>
+    </table>
+  );
+};
+export { Person };
